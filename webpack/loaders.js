c5eafff8280596c02c7dd957b89357c832a33b3a
Nokey <bi.jiankun@cgtn.com>
2018-03-20 15:53:16 +0800

    U: preset-2015 -> env

--- webpack/loaders.js
+++ webpack/loaders.js
@@ -19,7 +19,14 @@ module.exports = {
             ],
             loader: 'babel-loader',
             query: {
-                presets: ['es2015', 'react'],
+                presets: [
+                    ['env', {
+                        'targets': {
+                            'browsers': ['last 2 versions', 'ie >= 9']
+                        }
+                    }], 
+                    'react'
+                ],
                 cacheDirectory: false
             }
         },
